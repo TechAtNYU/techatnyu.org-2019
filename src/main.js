81f417c8b6664616bcd208cf984b1426bb926767
Adisa Narula <an1519@nyu.edu>
2017-02-06 15:07:29 -0500

    setting up

--- src/main.js
+++ src/main.js
@@ -1,10 +1,4 @@
+require('../stylesheets/main.scss');
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './Counter';
- 
-document.addEventListener('DOMContentLoaded', function() {
-  ReactDOM.render(
-    React.createElement(Counter),
-    document.getElementById('mount')
-  );
-});
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
  
 var config = {
   context: path.join(__dirname, 'src'),
@@ -11,13 +12,15 @@ var config = {
   },
   module: {
     loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loaders: ['babel'],
-      },
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      { test: /\.scss$/, loader: ExtractTextPlugin.extract('css!sass')}
     ],
   },
+  plugins: [
+        new ExtractTextPlugin('stylesheets/main.css', {
+            allChunks: true
+        })
+    ],
   resolveLoader: {
     root: [
       path.join(__dirname, 'node_modules'),
@@ -30,3 +33,4 @@ var config = {
   },
 };
 module.exports = config;
+
