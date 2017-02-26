46fd9d506e1e9951138964245262ab80d54e34ce
Adisa Narula <an1519@nyu.edu>
2017-02-26 15:02:05 -0500

    finished text/css for faq page

--- src/Carousel.js
+++ src/Carousel.js
@@ -5,7 +5,7 @@ import React from 'react';
 import Slider from 'react-slick';
 
 // TODO:: Create dynamic event cards
-// TODO:: Custom arrows and fix shadows
+// TODO:: Custom arrows
 
 var SimpleSlider = React.createClass({
 
@@ -34,26 +34,38 @@ var SimpleSlider = React.createClass({
                     </div>
                     <div>
                         <div className="event-card">
-                            <p className="event-title"> GHC MEETUP </p>
-                            <p className="event-team"> After Hours </p>
+                            <p className="event-title"> INTRO TO NODE JS W/JOEL </p>
+                            <p className="event-team"> Hack Days </p>
                             <p className="event-description">
-                                After Hours is back with the GHC Edition—anybody attending Grace Hopper from NYU is
-                                welcome to attend so we can all get to meet each other before the celebration.
+                                Joel Kemp is a software engineer at Spotify.
+                                The purpose of this workshop is to showcase the basics of the server-side language.
                             </p>
                             <hr></hr>
-                            <p className="event-date"> 10.12 </p>
+                                <p className="event-date"> 10.17 </p>
                         </div>
                     </div>
                     <div>
                         <div className="event-card">
-                            <p className="event-title"> GHC MEETUP </p>
-                            <p className="event-team"> After Hours </p>
+                            <p className="event-title"> REPORTING WITH VISUALS </p>
+                            <p className="event-team"> Design Days </p>
                             <p className="event-description">
-                                After Hours is back with the GHC Edition—anybody attending Grace Hopper from NYU is
-                                welcome to attend so we can all get to meet each other before the celebration.
+                                Larry Buchanan, graphics editor at the The New York Times,
+                                will be leading a talk about covering the news visually.
                             </p>
-                            <hr></hr>
-                            <p className="event-date"> 10.12 </p>
+                            <hr ></hr>
+                                <p className="event-date"> 10.17 </p>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="event-card">
+                            <p className="event-title"> INTRO TO UNITY </p>
+                            <p className="event-team"> Game Days </p>
+                            <p className="event-description">
+                                Larry Buchanan, graphics editor at the The New York Times,
+                                will be leading a talk about covering the news visually.
+                            </p>
+                            <hr ></hr>
+                            <p className="event-date"> 10.20 </p>
                         </div>
                     </div>
                 </Slider>
@@ -61,7 +73,6 @@ var SimpleSlider = React.createClass({
         );
     }
 });
-
 
 
 export default SimpleSlider;
