23304b063033e1d4885d2054345d732e6eb65f4b
Adisa Narula <an1519@nyu.edu>
2017-04-03 16:14:12 -0400

    moved navigation bar

---
+++ src/Navs.js
@@ -0,0 +1,51 @@
+/**
+ * Created by adisanarula on 2/28/17.
+ */
+ 
+import React from 'react';
+import constantLinks from './Constants';
+
+var Header = React.createClass ({
+
+    render: function () {
+        return(
+            <nav className="navbar-header">
+                <div className="navbar-content">
+                    <ul>
+                        <li><a href="index.html">home</a></li>
+                        <li ><a href="about.html">about</a></li>
+                        <li><a href="faq.html">faq</a></li>
+                        <li><a href="jobs.html">jobs</a></li>
+                        <li><a href="library.html">library</a></li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+});
+
+var Footer = React.createClass({
+    render: function() {
+        return(
+            <div>
+                <hr></hr>
+                <div id="footer-content">
+                    <div id="copyright"> ©Tech@NYU, 2016 </div>
+                    <div id="social-handles">
+                        <a href = {constantLinks.facebook} target="_blank">Facebook</a>
+                        <a href= {constantLinks.twitter} target="_blank">Twitter</a>
+                        <a href= {constantLinks.medium} target="_blank">Medium</a>
+                        <a href= {constantLinks.instagram} target="_blank">Instagram</a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
+
+const Navigation = {
+    header:Header,
+    footer: Footer
+}
+
+export default Navigation;
