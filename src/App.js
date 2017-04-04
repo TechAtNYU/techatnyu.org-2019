75756e159ad8a8d2fe74d1c4635ee69cd74a5f67
Adisa Narula <an1519@nyu.edu>
2017-04-04 14:55:27 -0400

    frustrations

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
-import React, { Component } from 'react'
-import { Router, Route, Link, IndexRoute, hashHistory, browserHistory, IndexLink } from 'react-router'
+import React from 'react';
+import { BrowserRouter as Router, Link, Match, Miss } from 'react-router';
+import Home from './Home';
 
-
-
-const Header = () => (
-    <nav className="navbar-header">
-        <div className="navbar-content">
-            <ul>
-                <li><a href="index.html">home</a></li>
-                <li ><a href="about.html">about</a></li>
-                <li><a href="faq.html">faq</a></li>
-                <li><a href="jobs.html">jobs</a></li>
-                <li><a href="library.html">library</a></li>
-            </ul>
-        </div>
-    </nav>
-)
-
-// const Container = (prop)  => 
-// <div>
-//     <Navigation.header />
-//     {props.children}
-// </div> 
-
-
-class App extends Component {
-    render() {
-        return (
-            <div>
-            	<Header />
-            </div>
-        )
-    }
-}
-
-
-const Home = () => <h1> Hello from Home! </h1>
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+        </ul>
+        <hr/>
+          <Match exactly pattern='/' component={Home}/>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
