5776d9bd2c0462a1f85665990d0bfc814ddf6c98
Adisa Narula <an1519@nyu.edu>
2017-04-04 17:12:54 -0400

    it worked

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React from 'react';
-import { BrowserRouter as Router, Link, Match, Miss } from 'react-router';
+import React, { Component } from 'react'
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory, IndexLink } from 'react-router'
 import Home from './Home';
 
-const App = () => {
-  return (
-    <Router>
-      <div>
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-        </ul>
-        <hr/>
-          <Match exactly pattern='/' component={Home}/>
-      </div>
-    </Router>
-  );
-};
+
+const Nav = () => (
+         <nav className="navbar-header">
+                <div className="navbar-content">
+                    <ul>
+                        <li><IndexLink  activeClassName="active" to="/">home</IndexLink></li>
+                        <li><IndexLink  activeClassName="active" to="/about">about</IndexLink></li>
+                        <li><IndexLink  activeClassName="active" to="/faq">faq</IndexLink></li>
+                        <li><IndexLink  activeClassName="active" to="/jobs">jobs</IndexLink></li>
+                        <li><IndexLink  activeClassName="active" to="/library">library</IndexLink></li>
+                    </ul>
+                </div>
+        </nav>
+)
+
+const Container = (props) => <div>
+    <Nav />
+    {props.children}
+</div>
+
+class App extends Component {
+    render() {
+        return (
+            <Router history={hashHistory}>
+                <Route path="/" component = {Container}>
+                    <IndexRoute component={Home} />
+                </Route>
+            </Router>
+        )
+    }
+}
 
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,46 @@
 import React from 'react';
 
-const Home = () => {
-    return (
-        <div>
-            <h2>Home</h2>
-        </div>
-    );
-};
+
+var EmailForm = React.createClass({
+	getInitialState: function() {
+	    return {
+	      value: "Type your email"
+	    };
+  	},
+  	handleChange: function(evt) {
+	    this.setState({
+	      value: evt.target.value
+	    });
+	},
+	submitChange: function(email) {
+		console.log(email); // TODO: Connect to backend
+	},
+	render: function() {
+		return(
+			<div id="signup-form">
+			        <p> Be the first to know about upcoming events and opportunities. </p><br/>
+			        <form>
+			        	<input type="email" value={this.state.value} onChange={this.handleChange}/>
+				        <button onClick={()=>this.submitChange(this.state.value)} type="submit" className="btn btn-success">
+		                	<i className="material-icons">arrow_forward</i>
+	            		</button>
+			        </form>
+	    	</div>
+		);
+	}
+});
+
+var Home = React.createClass({
+    render: function() {
+    	return (
+	        <div>
+	            <div id="tnyu-title">
+	            	<h1>tech@nyu</h1>
+			    </div>
+			    <EmailForm/>
+	        </div>
+	  );
+    }
+});
 
 export default Home;
