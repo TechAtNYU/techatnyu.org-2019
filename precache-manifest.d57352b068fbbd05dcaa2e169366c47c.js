ca37cba0401e4f2c8bc7eba449dda47f9419222a
Michael Yang <themichaelyang@gmail.com>
2019-02-27 16:47:17 -0500

    Updates

--- precache-manifest.d57352b068fbbd05dcaa2e169366c47c.js
+++ precache-manifest.d57352b068fbbd05dcaa2e169366c47c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "bdae78cdb4c182321a70",
+    "revision": "e21ae56cb46e616aef32",
     "url": "/static/css/main.33ee8eed.chunk.css"
   },
   {
-    "revision": "bdae78cdb4c182321a70",
-    "url": "/static/js/main.bdae78cd.chunk.js"
+    "revision": "e21ae56cb46e616aef32",
+    "url": "/static/js/main.e21ae56c.chunk.js"
   },
   {
     "revision": "fdfcfda2d9b1bf31db52",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/static/js/2.f76c6dc5.chunk.js"
   },
   {
-    "revision": "f1b33581de50f7e29578b7041027a0c7",
+    "revision": "8953c27a178d5cd397cdf2a177731088",
     "url": "/index.html"
   }
 ];
